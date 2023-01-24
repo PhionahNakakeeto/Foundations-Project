3f179ab169958b8a87c4b046966fa32ac1ba39be
phionah nakakeeto <nakakeetof@gmail.com>
2023-01-24 15:17:52 -0800

    Modified Features

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {submitTicket, retrieveAllTickets,
     retrieveTicketsByStatus, retrieveTicketsById, updateTicketsById,
      retrieveTicketsByUsername} = require('./DAO/tickets-dao');
 const timestamp = require('unix-timestamp');
+const uuid = require('uuid')
 timestamp.round = true     
 
 // Creating server
@@ -61,6 +62,7 @@ try{
     const data = await retrieveUserByUsername(username);
     const userItem = data.Item;
     if(userItem){
+        res.statusCode = 400;
         res.send({
             "message" : "User already exists"
         })
@@ -142,7 +144,7 @@ app.post('/submitticket', async(req, res) => {
         const payload = await verifyTokenAndReturnPayload(token);
         if(payload.role === 'employee'){
            try{
-                await submitTicket(req.body.ticket_id, payload.username, timestamp.now(), req.body.amount, req.body.desc);
+                await submitTicket(uuid.v4(), payload.username, timestamp.now(), req.body.amount, req.body.desc);
                 res.send({
                     "message" : "Successfully submitted Ticket"
                 })
@@ -154,8 +156,9 @@ app.post('/submitticket', async(req, res) => {
             }
               
         } else {
+            res.statusCode = 400;
             res.send({
-                "message" : "You're not a regular employee"
+                 "message" : "You're not a regular employee"
             })
         }
     }
@@ -170,7 +173,7 @@ app.post('/submitticket', async(req, res) => {
 
 })
 
-// Endpoint for Allowing employees to retrieve previous tickets by username
+// Endpoint for Allowing employees to retrieve previous tickets
 
 app.get('/tickets', async(req, res) => {
     const token = req.headers.authorization.split(' ')[1];
@@ -178,22 +181,24 @@ app.get('/tickets', async(req, res) => {
    try{
     const payload = await verifyTokenAndReturnPayload(token);
     if(payload.role === 'employee'){
-        if(req.query.status){
-        let data = await retrieveTicketsByUsername(req.query.username);
+       
+        let data = await retrieveTicketsByUsername(payload.username);
         res.send(data.Items);
 
     }else{
-        let data = await retrieveAllTickets();
-        res.send(data.Items);
-    }
-}else {
-    res.send({
-        "message" : "You're not a regular employee"
-    })
-
+        if(req.query.status){
+            let data = await retrieveTicketsByStatus(req.query.status);
+            res.send(data.Items);
+        
+         } else{
+             let data = await retrieveAllTickets();
+            res.send(data.Items);
+        }
+        
     }
 
    }
+   
    catch(err){
     res.statusCode = 500;
     res.send({
@@ -204,55 +209,55 @@ app.get('/tickets', async(req, res) => {
 
 // Endpoint for Allowing managers to retrieve previous tickets by status
 
-app.get('/ticketsbystatus', async(req, res) => {
-    const token = req.headers.authorization.split(' ')[1];
+// app.get('/ticketsbystatus', async(req, res) => {
+//     const token = req.headers.authorization.split(' ')[1];
     
-   try{
-    const payload = await verifyTokenAndReturnPayload(token);
-    if(payload.role === 'admin'){
-        if(req.query.status === 'pending'){
-        let data = await retrieveTicketsByStatus(req.query.status);
-        res.send(data.Items);
+//    try{
+//     const payload = await verifyTokenAndReturnPayload(token);
+//     if(payload.role === 'admin'){
+//         if(req.query.status === 'pending'){
+//         let data = await retrieveTicketsByStatus(req.query.status);
+//         res.send(data.Items);
 
-    }else{
-        let data = await retrieveAllTickets();
-        res.send(data.Items);
-    }
-}else {
-    res.send({
-        "message" : "You're not an Admin"
-    })
+//     }else{
+//         let data = await retrieveAllTickets();
+//         res.send(data.Items);
+//     }
+// }else {
+//     res.send({
+//         "message" : "You're not an Admin"
+//     })
 
-    }
+//     }
 
 
-   }
-   catch(err){
-    res.statusCode = 500;
-    res.send({
-        "message" : err
-    })
-   }
-})
+//    }
+//    catch(err){
+//     res.statusCode = 500;
+//     res.send({
+//         "message" : err
+//     })
+//    }
+// })
 
-app.get('/ticketsbyid/:ticket_id', async(req, res) => {
-    try{
-        let data = await retrieveTicketsById(req.params.ticket_id);
-        if(data.Item){
-            res.send(data.Item)
-        } else {
-            res.statusCode = 404
-            res.send({
-                "message" : "Ticket doesn't exist"
-            });
-        }
+// app.get('/ticketsbyid/:ticket_id', async(req, res) => {
+//     try{
+//         let data = await retrieveTicketsById(req.params.ticket_id);
+//         if(data.Item){
+//             res.send(data.Item)
+//         } else {
+//             res.statusCode = 404
+//             res.send({
+//                 "message" : "Ticket doesn't exist"
+//             });
+//         }
        
-    } catch(err){
-        res.statusCode = 500
-        res.send()
-    }
+//     } catch(err){
+//         res.statusCode = 500
+//         res.send()
+//     }
    
-})
+// })
 
 // Endpoint for approving/denying reimbursement tickets
 
